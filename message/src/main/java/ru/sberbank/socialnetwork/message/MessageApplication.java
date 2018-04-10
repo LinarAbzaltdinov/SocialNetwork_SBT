5089aca9a2f8c6ec09078061a082087c22768da6
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-04-10 15:11:25 +0300

    Added Message MS. Updated all pom.xml(deleted version of spring boot). Updated dockerfiles

---
+++ message/src/main/java/ru/sberbank/socialnetwork/message/MessageApplication.java
@@ -0,0 +1,12 @@
+package ru.sberbank.socialnetwork.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+}
