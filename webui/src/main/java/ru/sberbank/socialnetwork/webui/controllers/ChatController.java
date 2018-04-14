7eeabee4f9435e786762470cf563332fb5130053
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-04-15 01:16:33 +0300

    Added page for list of groups.

---
+++ webui/src/main/java/ru/sberbank/socialnetwork/webui/controllers/ChatController.java
@@ -0,0 +1,12 @@
+package ru.sberbank.socialnetwork.webui.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+    @GetMapping("/groups")
+    public String showGroups() {
+        return "groups";
+    }
+}
