f683e47b42031b9f44553ae37ac6ae730c9d7f11
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-04-18 00:17:42 +0300

    Added new service eureka-server.

---
+++ eureka-server/src/main/java/ru/sberbank/socialnetwork/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package ru.sberbank.socialnetwork.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
