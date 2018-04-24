0b84b703f6da1bbf5d1d7eb9c2ffa223dea4bc93
Linar Abzaltdinov <abzaltdinov@gmail.com>
2018-04-24 15:51:49 +0300

    added id to GroupUser

--- chat/src/main/java/ru/sberbank/socialnetwork/chat/dao/GroupRepository.java
+++ chat/src/main/java/ru/sberbank/socialnetwork/chat/dao/GroupRepository.java
@@ -13,8 +13,8 @@ public interface GroupRepository extends Repository<Group, Long> {
 
     Group findById(Long id);
 
-    @Query("select g from Group g where g.users.uuid = :#{#uuid}")
-    Collection<Group> findByUsersUuid(@Param("uuid")String uuid);
+    @Query("select g from Group g where g.users.uuid = :uuid")
+    Collection<Group> findByUsersUuid(@Param("uuid") String uuid);
 
     Collection<Group> findByGroupNameStartingWith(String prefix);
 
