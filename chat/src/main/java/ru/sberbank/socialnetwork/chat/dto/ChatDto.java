b7cac6c3c4c7f63b7c8a123f964726247b6e4727
KovalNikita <koval.nikita.95@mail.ru>
2018-04-24 19:17:17 +0300

    Chats-Messeges interaction was added

--- chat/src/main/java/ru/sberbank/socialnetwork/chat/dto/ChatDto.java
+++ chat/src/main/java/ru/sberbank/socialnetwork/chat/dto/ChatDto.java
@@ -1,7 +1,11 @@
 package ru.sberbank.socialnetwork.chat.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import ru.sberbank.socialnetwork.chat.entities.Chat;
+
+import java.util.Collection;
 
 @AllArgsConstructor
 public class ChatDto {
@@ -9,10 +13,14 @@ public class ChatDto {
     private String chatName;
     private String creatorId;
     private Long groupId;
+    @Getter @Setter
+    private Collection<MessageDto> messages;
+
     public ChatDto(Chat chat) {
         this.id = chat.getId();
         this.chatName = chat.getChatName();
         this.creatorId = chat.getCreatorId();
         this.groupId = chat.getGroup().getId();
+        this.messages = null;
     }
 }
